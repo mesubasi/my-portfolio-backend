01b2685d197163a9ecca2d59b27b2f27e348fbf0
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-06-05 22:39:52 +0300

    Contact GET Added

--- routes/contact.js
+++ routes/contact.js
@@ -13,4 +13,14 @@ router.post("/to-send", async (req, res) => {
   }
 });
 
+//GET All Message
+router.get("/get-all-message", async (req, res) => {
+  try {
+    const allMessage = await Contact.find();
+    res.status(200).json(allMessage)
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
